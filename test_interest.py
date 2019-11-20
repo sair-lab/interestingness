fix boxbar range bug, add moving average

test_interest.py
@@ -48,9 +48,10 @@ from torchvision.datasets import CocoDetection
 from torch.utils.tensorboard import SummaryWriter
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
-from dataset import ImageData, Dronefilm, DroneFilming, SubT, SubTF, PersonalVideo
 from interestingness import AE, VAE, AutoEncoder, Interestingness
-from torchutil import count_parameters, show_batch, show_batch_origin, ConvLoss, CosineLoss, CorrelationLoss, Split2d, Merge2d, PearsonLoss, FiveSplit2d
+from dataset import ImageData, Dronefilm, DroneFilming, SubT, SubTF, PersonalVideo
+from torchutil import count_parameters, show_batch, show_batch_origin, Timer, MovAvg
+from torchutil import ConvLoss, CosineLoss, CorrelationLoss, Split2d, Merge2d, PearsonLoss, FiveSplit2d
 
 class Interest():
     '''
@@ -84,16 +85,19 @@ class Interest():
 
 
 def performance(loader, net):
-    test_loss = 0
+    test_loss, time_use = 0, 0
     with torch.no_grad():
         for batch_idx, inputs in enumerate(loader):
             if batch_idx % args.skip_frames !=0:
                 continue
             if torch.cuda.is_available():
                 inputs = inputs.cuda()
+            timer.tic()
             inputs = Variable(inputs)
             outputs, loss = net(inputs)
-            if args.debug is not True:
+            loss = movavg.append(loss)
+            time_use += timer.end()
+            if args.debug is True:
                 drawbox(inputs, outputs)
             test_loss += loss.item()
             frame = show_batch_box(inputs, batch_idx, loss.item())
@@ -105,15 +109,15 @@ def performance(loader, net):
                 cv2.imwrite('images/%s-%d/%s-reconstruction-%06d.png'%(args.dataset,args.test_data,args.save_flag,batch_idx), image*255)
                 cv2.imwrite('images/%s-%d/%s-difference-%06d.png'%(args.dataset,args.test_data,args.save_flag,batch_idx), recon*255)
             print('batch_idx:', batch_idx, 'loss:%.6f'%(loss.item()))
-
+    print("Total time using: %fs, %fms/frame"%(time_use, 1000*time_use/(batch_idx+1)))
     cv2.imwrite('results/%s.png'%(test_name), 255*top_interests)
     return test_loss/(batch_idx+1)
 
 
-def boxbar(height, bar, ranges=[0, 0.1], threshold=[0.05, 0.06]):
+def boxbar(height, bar, ranges=[0.04, 0.08], threshold=[0.05, 0.06]):
     width = 15
     box = np.zeros((height,width,3), np.uint8)
-    x1, y1 = 0, int((1.0-bar/(ranges[1]-ranges[0]))*height)
+    x1, y1 = 0, int((1.0-(bar-ranges[0])/(ranges[1]-ranges[0]))*height)
     x2, y2 = int(width), int(height)
     cv2.rectangle(box,(x1,y1),(x2,y2),(0,1,0),-1)
     for i in threshold:
@@ -143,13 +147,14 @@ if __name__ == "__main__":
     # Arguements
     parser = argparse.ArgumentParser(description='Test Interestingness Networks')
     parser.add_argument("--data-root", type=str, default='/data/datasets', help="dataset root folder")
-    parser.add_argument("--model-save", type=str, default='saves/ae.pt.DroneFilming.interest.mse', help="learning rate")
+    parser.add_argument("--model-save", type=str, default='saves/ae.pt.DroneFilming.interest.mse', help="read model")
     parser.add_argument("--test-data", type=int, default=0, help='test data ID.')
     parser.add_argument("--seed", type=int, default=0, help='Random seed.')
     parser.add_argument("--crop-size", type=int, default=320, help='loss compute by grid')
     parser.add_argument("--num-interest", type=int, default=10, help='loss compute by grid')
     parser.add_argument("--skip-frames", type=int, default=1, help='skip frame')
-    parser.add_argument('--dataset', type=str, default='PersonalVideo', help='dataset type (subT ot drone')
+    parser.add_argument("--window-size", type=int, default=1, help='smooth window size >=1')
+    parser.add_argument('--dataset', type=str, default='DroneFilming', help='dataset type (SubTF, DroneFilming, or PersonalVideo')
     parser.add_argument('--save-flag', type=str, default='interests', help='save name flat')
     parser.add_argument("--rr", type=float, default=5, help="reading rate")
     parser.add_argument("--wr", type=float, default=5, help="reading rate")
@@ -171,6 +176,7 @@ if __name__ == "__main__":
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
 
+    timer = Timer()
     test_name = '%s-%d-%s-%s'%(args.dataset, args.test_data, time.strftime('%Y-%m-%d-%H:%M:%S'), args.save_flag)
 
     if args.dataset == 'DroneFilming':
@@ -187,6 +193,7 @@ if __name__ == "__main__":
     net.memory.set_learning_rate(rr=args.rr, wr=args.wr)
 
     interest = Interest(args.num_interest, 'results/%s.txt'%(test_name))
+    movavg = MovAvg(args.window_size)
     if torch.cuda.is_available():
         net = net.cuda()
 